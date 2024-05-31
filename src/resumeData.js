3703488a95d51e861f391ec122ecfb973be1efc8
saad1937-berra <saadberra11@gmail.com>
2024-05-31 10:55:08 +0100

    first commit

---
+++ src/resumeData.js
@@ -0,0 +1,73 @@
+let resumeData = {
+    "imagebaseurl":"https://rbhatia6.github.io/",
+    "name": "Saad Berra",
+    "role": "Full stack Developper",
+    "linkedinId":"Your LinkedIn Id",
+    "roleDescription": "I like dabbling in various parts of frontend and backend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/saad-berra-0842412ab/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/saad1937-berra/",
+          "className":"fa fa-github"
+        },
+      ],
+    "aboutme":"I am currently a pre-final year student at The Eurelec Institute of Information Technology and pursuing my B.Tech from here. I am a self taught Full Stack Web Developer, currently diving deeper into Machine Learning. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
+    "address":"Morocco",
+    "education":[
+      {
+        "UniversityName":"Eurelec",
+        "specialization":"Developpement Web",
+        "MonthOfPassing":"Aug",
+        "YearOfPassing":"2024",
+        "Achievements":"Realising some projets for school and some freelances"
+      },
+      {
+        "UniversityName":"Khansae",
+        "specialization":"Economic sciences",
+        "MonthOfPassing":"July",
+        "YearOfPassing":"2020",
+        "Achievements":"Baccalaureat with mention Good"
+      }
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"Website",
+        "imgurl":"images/portfolio/Capture.png"
+      },
+      {
+        "name":"project2",
+        "description":"Webite",
+        "imgurl":"images/portfolio/Capture2.png"
+      },
+      {
+        "name":"project3",
+        "description":"Website",  
+        "imgurl":"images/portfolio/Capture3.png"
+      },
+      {
+        "name":"project4",
+        "description":"E-Commerce",
+        "imgurl":"images/portfolio/Capture4.png"
+      }
+    ],
+  }
+  
+  export default resumeData
